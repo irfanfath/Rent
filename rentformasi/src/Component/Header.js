714f76579a623102e78070f1a56f2e35562a3423
irfanfath <mirfanf7@gmail.com>
2020-03-10 11:57:49 +0700

    Update Header.js

--- rentformasi/src/Component/Header.js
+++ rentformasi/src/Component/Header.js
@@ -31,41 +31,15 @@ class Header extends Component{
                                 <NavLink to="/barang" className="nav-link w-nav-link">Sewa Barang</NavLink>
                                 <NavLink to="/jasa" className="nav-link w-nav-link">Sewa Jasa</NavLink>
                                 <NavLink to="/bantuan" className="nav-link w-nav-link">Bantuan</NavLink>
-                                <NavLink to="/coba"><i className="material-icons">cart</i></NavLink>
+                                {/* <NavLink to="/coba"><i className="material-icons">cart</i></NavLink> */}
                             </nav>
                             <div className="w-commerce-commercecartopenlink cart-button w-inline-block">
-                                <NavLink to="/wish" className="w-inline-block">Wish List</NavLink>
+                                <NavLink to="/wish" className="w-inline-block">WishList</NavLink>
                                 <img src="https://img.icons8.com/material-outlined/24/000000/wish-list.png" alt="" className="cart-icon" onClick={() => this.toggleModal("exampleModal")} />
                                 <div className="w-commerce-commercecartopenlinkcount item-count" onClick={() => this.toggleModal("exampleModal")}>0</div>
                             </div>
-                                {/* <Modal
-                                    className="modal-dialog-centered"
-                                    isOpen={this.state.exampleModal}
-                                    toggle={() => this.toggleModal("exampleModal")}
-                                    >
-                                    <div className="modal-header">
-                                        <h5 className="modal-title" id="exampleModalLabel">
-                                        Wish List Anda
-                                        </h5>
-                                        <button
-                                        aria-label="Close"
-                                        className="close"
-                                        data-dismiss="modal"
-                                        type="button"
-                                        onClick={() => this.toggleModal("exampleModal")}
-                                        >
-                                        <span aria-hidden={true}>×</span>
-                                        </button>
-                                    </div>
-                                    <CartValue/>
-                                    <div className="modal-footer">
-                                        <NavLink to="/keranjang" className="nav-link-product w-nav-link">
-                                            <input type="submit" value="Lihat Semua" className="w-commerce-commerceaddtocartbutton button" onClick={() => this.toggleModal("exampleModal")}/>
-                                        </NavLink>
-                                    </div>
-                                </Modal> */}
                         </div>
-                        <div className="nav-link w-nav-link" onClick={() => this.toggleModal("loginModal")}><ButtonLogin/>
+                        <div className="w-commerce-commercecartopenlink cart-button w-inline-block" onClick={() => this.toggleModal("loginModal")}><ButtonLogin/>
                             <Modal
                                 className="modal-dialog-centered"
                                 isOpen={this.state.loginModal}
