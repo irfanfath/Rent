f9165a786291e003c00ffc1ab697579b35a1a07f
irfanfath <mirfanf7@gmail.com>
2020-07-15 11:43:44 +0700

    Fixing WishList (Jadi keranjang)

--- rentformasi/src/Views/WishList.js
+++ rentformasi/src/Views/WishList.js
@@ -37,18 +37,30 @@ class WishList extends Component{
                     <div className="wrapper">
                         <div className="contacts">
                             <div className="form-wishlist">
-                                <h4 className="contact-heading">Daftar Wish List Anda : </h4>
-                                <div>
-                                    {
-                                        this.state.post.map(post => {
-                                            return <ListWish key={post.id}
-                                            data={post}
-                                            />
-                                        })
-                                    }
+                                <h4 className="contact-heading">Keranjang Anda : </h4>
+                                <div className="w-commerce-keranjang">
+                                    <div className="keranjang-img"> 
+                                        <img src="https://www.otomotifer.com/wp-content/uploads/2019/04/Mobil-City-Car-Murah-Honda.jpg" alt=""/>
+                                    </div>
+                                    <div className="w-commerce-produk">
+                                        <div className="title-keranjang">eughuehfwhiwfefwewfewfw</div>
+                                        <div className="harga-keranjang">Harga : Rp. 5000</div> 
+                                        {/* <button className="cart-remove-link">Hapus Dari Daftar</button> */}
+                                    </div>    
+                                    <div className="w-form-date">
+                                        <form className="subscribe-form-date">
+                                            <div className="title-date">Tanggal Sewa
+                                                <input type="date" className="input subscribe-keranjang w-input" placeholder="Tanggal Sewa" id="tanggalsewa" required="" />
+                                            </div>
+                                            <div className="title-date">Tanggal Kembali
+                                                <input type="date" className="input subscribe-keranjang w-input" placeholder="Tanggal Kembali" id="tanggalkembali" required="" />
+                                            </div>
+                                        </form>
+                                    </div>                                
                                 </div>
                                 <Recipe/>
                             </div>
+                            <button className="button-full w-half">Bayar Sekarang</button>
                         </div>
                     </div>
                 </div>
